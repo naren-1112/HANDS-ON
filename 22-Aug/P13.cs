714656c19c80e50bc79cbafa039a1ac14d57276e
NAREN S K <111351790+naren-1112@users.noreply.github.com>
2022-08-23 00:02:38 +0530

    Add files via upload

---
+++ 22-Aug/P13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13
+{
+    class P13
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 3, 5, 200, 22, 2244, 213, 111 };
+            int r = 100;
+            Console.WriteLine("Given Integer {0}: ", r);
+            Console.WriteLine("Numbers larger than {0} = ", r);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > r)
+                {
+                    Console.WriteLine(arr[i]);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
