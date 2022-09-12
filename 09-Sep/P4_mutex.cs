913b3762b6c29b7ce35f0e8915cbf7c9891c0f9a
NAREN S K <111351790+naren-1112@users.noreply.github.com>
2022-09-12 12:07:22 +0530

    Add files via upload

---
+++ 09-Sep/P4_mutex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace P4_mutex
+{
+    class Program
+    {
+        class MutexDemo
+        {
+            static void Main(string[] args)
+            {
+                //imterprocess
+                using (Mutex mutex = new Mutex(false, "ThreadingDemo"))
+                {
+
+                    if (!mutex.WaitOne(1000, false))
+                    {
+                        Console.WriteLine("already running....");
+                        Console.ReadLine();
+                        return;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("app is running");
+                        Console.ReadLine();
+                    }
+
+                }
+
+            }
+        }
+    }
+}
+
