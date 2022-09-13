84aaf73c108d1ccae47be1745f05f806bbbe4969
NAREN S K <111351790+naren-1112@users.noreply.github.com>
2022-09-13 12:10:38 +0530

    Update P(b).cs

--- 13-Sep/P(b).cs
+++ 13-Sep/P(b).cs
@@ -52,27 +52,43 @@ namespace P_B
         }
     }
 }
+//Attribute
 
+System.AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    sealed class DeveloperAttribute : Attribute
+    {
+
+        public DeveloperAttribute(string songname)
+        {
+
+            this.SongName = songname;
+
+        }
+        public string SongName { get; set; }
+        public string Language { get; set; }
+        public string Lyrics { get; set; }
+    }
+}
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _12SEP_HANDSON
+namespace P_B
 {
-    [Developer("New Album", SongName = "Love in the air")]
+    [Developer("New Album", SongName = "PS1")]
     public class Song
     {
-        [Developer("Name of the song", SongName = "Fitoor")]
+        [Developer("Name of the song", SongName = "Ponni Nadhi")]
         public string SongName { get; set; }
 
-        [Developer("Language of the song", Language = "Hindi")]
+        [Developer("Language of the song", Language = "Tamil")]
         public string Language { get; set; }
 
-        [Developer("Lyrics", Lyrics = "abc")]
+        [Developer("Lyrics", Lyrics = "Vivek")]
 
-        [Developer("Language of the song", Language = "Hindi")]
+        [Developer("Language of the song", Language = "Tamil")]
         public string Lyrics { get; set; }
 
 
