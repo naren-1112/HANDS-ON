d32203f0f437c4a3e4564eb7fdbce09619d4eec0
NAREN S K <111351790+naren-1112@users.noreply.github.com>
2022-09-16 13:34:51 +0530

    Update P1.cs

--- 18-Aug/P1.cs
+++ 18-Aug/P1.cs
@@ -1,33 +1,36 @@
-using System;
+sing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace P10
+namespace P1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Input first integer: ");
-            int n1 = int.Parse(Console.ReadLine());
-            Console.Write("Input second integer: ");
-            int n2 = int.Parse(Console.ReadLine());
+            
+            int n1 = 5;
+            int n2 = 6;
 
-            //Check if one is negative and one is positive
+            // USING 3 VARIABLES
+            //int t;
+            //t = n1;
+            //n1 = n2;
+            //n2 = t;
 
-            //bool result = (n2 < 0 && n1 >= 0) || (n1 < 0 && n2 >= 0);
+            //// USING 2 VARIABLES
+            //n1 = n1 + n2;
+            //n2 = n1 - n2;
+            //n1 = n1 - n2;
 
-            //Console.WriteLine($"Check if one is negative and one is positive: {result}");
-
-            //Check if both are positive
-
-            bool result = (n2 >= 0 && n1 >= 0) && (n1 >= 0 && n2 >= 0);
-
-            Console.WriteLine($"Check if both are positive: {result}");
-
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
             Console.ReadLine();
+
+
         }
     }
 }
+
